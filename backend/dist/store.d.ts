8c2d026195659a4e3cc8e181c43315679f489e56
JAY PRAJAPATI <jayp37679@gmail.com>
2026-08-27 18:34:52 +0530

    chore: remove generated files and secrets

--- backend/dist/store.d.ts
+++
@@ -1,7 +0,0 @@
-import { Assessment } from "./types";
-export declare function getAssessment(id: string): Assessment | undefined;
-export declare function setAssessment(assessment: Assessment): void;
-export declare function updateAssessment(id: string, updates: Partial<Assessment>): Assessment | undefined;
-export declare function deleteAssessment(id: string): boolean;
-export declare function getAllAssessments(): Assessment[];
-//# sourceMappingURL=store.d.ts.map
